5bf7db17a55a2c8308944acf663bf5b1902114e1
Johan Tamayo <68561687+Johan-18@users.noreply.github.com>
2022-01-31 21:05:18 -0600

    i added all ten questions and put them in the same for loop

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+const length = Math.max(monthlyExpenseQuestions.length, weeklyExpenseQuestions.length, annualExpenseQuestions.length);
+for(let i = 0; i < length; i++){
+    let answer = 0;
+
+    if(i < weeklyExpenseQuestions.length)
+    answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+
+    if(i < monthlyExpenseQuestions.length)
+    answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+
+    if(i < annualExpenseQuestions.length)
+    answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+
+}
